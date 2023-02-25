6a6ea49cd6aaaf9044c5ae0d69b7ca5e290b2640
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-02-25 17:46:58 +0500

    if02.py

--- if02.py
+++ if02.py
@@ -8,4 +8,19 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    dgf = a
+    if a < b:
+        if a < c:
+            dgf = a
+        else:
+            dgf = c
+    else:
+        if b < c:
+            dgf = b
+        else:
+            dgf = c
+
+    return dfg
+
+print(main(1,4,2))
+
